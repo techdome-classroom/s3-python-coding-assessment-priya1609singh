77d2eecdc96f2f60413fa47764eaf2d838ceec5d
priya1609singh <130379489+priya1609singh@users.noreply.github.com>
2024-10-07 15:29:05 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+       roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+  
+        total = 0
+        
+     
+        for i in range(len(s)):
+           
+            current_val = roman_map[s[i]]
+            
+           
+            if i + 1 < len(s) and current_val < roman_map[s[i + 1]]:
+                total -= current_val
+            else:
+                # Otherwise, add it
+                total += current_val
+                
+        return total
+
 
 
 
